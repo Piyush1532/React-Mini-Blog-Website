77df93d83b62aedee13dca8b59f94c95fd553906
Piyush1532 <piyushadav503@gmail.com>
2024-05-10 12:50:42 +0530

    footer ready

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <div>
+   <footer>
+    <div className="para">
+        <p>2024 -Piyush - All Rights Reserved</p>
+    </div>
+    <div className="icon">
+    <i className="fab fa-facebook-f"></i>
+    <i className="fab fa-instagram"></i>
+    <i class="fab fa-twitter"></i>
+    <i className="fab fa-youtube"></i>
+    </div>
+   </footer>
+    </div>
+  )
+}
+
+export default Footer
